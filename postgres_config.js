b83283531db3401c1ded9bd21f9a0d4d701906b1
diszszs <hadisrodpradid222@gmail.com>
2024-11-25 10:08:55 +0700

    first commit

---
+++ postgres_config.js
@@ -0,0 +1,14 @@
+
+const {Pool} = require("pg");
+
+const pool = new Pool(
+    {
+        host: "localhost",
+        user: "postgres",
+        database: "useAPI",
+        password: "postgres",
+        port: 5432,
+    }
+);
+
+module.exports = pool;
